5e5f30852aa850db395bcf4223a584cade58d8d5
nexckycort <nexckycort@gmail.com>
2025-05-18 13:51:46 -0500

    Add prompt for app choices in installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Ask for app choices
+echo "Get ready to make a few choices..."
+source ~/.local/share/oni-setup/install/terminal/required/app-gum.sh >/dev/null
+
 # Desktop software and tweaks will only be installed if we're running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   # Ensure computer doesn't go to sleep or lock while installing
