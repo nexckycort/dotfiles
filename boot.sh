f9dc02635ada31d74cc30fd7544752fca78d1a1e
nexckycort <nexckycort@gmail.com>
2025-05-18 18:42:11 -0500

    Move Git configuration check to boot.sh for improved installation flow

--- boot.sh
+++ boot.sh
@@ -6,6 +6,10 @@ echo -e "\nBegin installation (or abort with ctrl+c)..."
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
+if git config --global --get url.ssh://git@github.com/.insteadOf >/dev/null; then
+  git config --global --unset url.ssh://git@github.com/.insteadOf
+fi
+
 echo "Cloning Oni Setup..."
 rm -rf ~/.local/share/oni-setup
 git clone https://github.com/nexckycort/oni-setup.git ~/.local/share/oni-setup >/dev/null
--- install/terminal.sh
+++ install/terminal.sh
@@ -3,9 +3,5 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git unzip zsh
 
-if git config --global --get url.ssh://git@github.com/.insteadOf >/dev/null; then
-  git config --global --unset url.ssh://git@github.com/.insteadOf
-fi
-
 # Run terminal installers
 for installer in ~/.local/share/oni-setup/install/terminal/*.sh; do source $installer; done
