3b39cc8f3c1de3e693992e386fc0be5dae851794
nexckycort <nexckycort@gmail.com>
2025-05-18 18:10:36 -0500

    Add error handling and retry message to installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Give people a chance to retry running the installation
+trap 'echo "Omakub installation failed! You can retry by running: source ~/.local/share/oni-setup/install.sh"' ERR
+
 # Ask for app choices
 echo "Get ready to make a few choices..."
 source ~/.local/share/oni-setup/install/terminal/required/app-gum.sh >/dev/null
 source ~/.local/share/oni-setup/install/first-run-choices.sh
-# source ~/.local/share/oni-setup/install/identification.sh
+source ~/.local/share/oni-setup/install/identification.sh
 
 # Desktop software and tweaks will only be installed if we're running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
